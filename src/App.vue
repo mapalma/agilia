f831be5677d1d676feb7c313efe39ea6a64f8240
Alda Palma <alda.palma.luque@gmail.com>
2020-09-30 11:33:20 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,79 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>Users</h1>
+    <h2>Create New User</h2>
+     <form v-on:submit.prevent="createUser()">
+        <label for="newUserName">Name</label>
+        <input id="newUserName" v-model="newUser.name">
+        <button>Create New user</button>
+      </form>
+    <h2>Users</h2>
+    <ul v-for="user in users" :key="user.id">
+      <li>Name {{user.first_name}}, {{user.job}} 
+        <button v-on:click.prevent="deleteUser(user.id)">Delete</button>
+        <button v-on:click.prevent="loadUserToEdit(user.id)">Edit</button>
+        </li>
+    </ul>
+    <h2>Edit User</h2>
+    <form v-on:submit.prevent="editUser()">
+      <label for="editUserName">Name</label>
+      <input id="editUserName" v-model="user.name">
+      <button>Edit User</button>
+    </form>
+    
   </div>
+ 
 </template>
-
+<script>
+import axios from 'axios'
+export default {
+  data(){
+    return {
+      newUser: {
+        name: ''
+      },
+      user: {
+        name: '',
+        id: null
+      }
+    }
+  },
+  created(){
+    this.$store.dispatch('loadUsers');
+  },
+  computed:{
+    users() {
+      return this.$store.getters.getUsers
+    }
+  },
+  methods:{
+    loadUserToEdit(id){
+      this.editMode = true;
+      axios.get("https://reqres.in/api/users/" + id)
+      .then((result) => {
+        console.log('user to edit', result);
+        this.user.name = result.data.data.first_name;
+        this.user.id = result.data.data.id;
+      })
+    },
+    createUser(){
+      this.$store.dispatch('createUser', this.newUser)
+      this.newUser.user = '';
+    },
+    editUser(){
+      this.$store.dispatch('editUser', this.user)
+    },
+    deleteUser(id){
+      this.$store.dispatch('deleteUser', id)
+    }
+  }
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
@@ -28,5 +88,9 @@
       color: #42b983;
     }
   }
+}
+
+ul {
+
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,52 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    users:[]
+  },
+  getters:{
+    getUsers:(state)=>{
+      return state.users;
+    }
   },
   mutations: {
+    loadUsers(state, users){
+      state.users = users
+    }
+
   },
   actions: {
+    loadUsers({commit}){
+      axios.get("https://reqres.in/api/users?page=2")
+      .then((result) => {
+        commit('loadUsers', result.data.data)
+      })
+    },
+    createUser(context, user){
+      axios.post("https://reqres.in/api/users", {"name":user.name})
+      .then((result) => {
+        console.log('created');
+        console.log(result);
+      })
+    },
+    editUser(context, user) {
+      axios.put("https://reqres.in/api/users/" + user.id, user.name)
+      .then((result) => {
+        console.log('edited');
+        console.log(result);
+      })
+    },
+    deleteUser(context, id){
+      axios.get("https://reqres.in/api/users/" + id)
+      .then((result) => {
+        console.log('user deleted');
+        console.log(result.data.data);
+      })
+    }
   },
   modules: {
   }
